cacc987ed9b550b0006045f11ada62889712e3c6
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2022-05-27 00:45:52 +0200

    - updated InfoView

--- SwiftfulCharting/Views/InfoView.swift
+++ SwiftfulCharting/Views/InfoView.swift
@@ -11,24 +11,46 @@ struct InfoView: View {
     var body: some View {
         HStack {
             VStack(alignment: .leading, spacing: 20) {
-                Text("How to use?")
-                    .font(.title)
-                    .bold()
-                    .padding(.top, 30)
-                Text("Choose currency which you want to convert, provide proper amount and then currency to which you want to convert. Click generate chart.")
-                Text("After data has been downloaded from API, the chart will be generated.")
-                Text("You can zoom it and see particular values by clicking on any of it's fragments. What's more, you can change currency at any time and new chart will be generated.")
-                
-                HStack {
-                    Link(destination: URL(string: "https://rapidapi.com/natkapral/api/currency-converter5/")!) {
-                        Text("Currency Converter")
-                            .bold()
-                    }
-                    .offset(y: -10.5)
-                    Text("API provides all the data for chart generating.")
-                        .fixedSize(horizontal: false, vertical: true)
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("How to use?")
+                        .font(.title)
+                        .bold()
+                        .padding(.top, 30)
+                    Text("Choose currency which you want to convert, provide proper amount and then currency to which you want to convert. Click generate chart.")
+                    Text("After data has been downloaded from API, the chart will be generated.")
+                    Text("You can zoom it and see particular values by clicking on any of it's fragments. What's more, you can change currency at any time and new chart will be generated.")
                 }
-                .font(.callout)
+                .padding(.bottom, 30)
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("3rd party software")
+                        .font(.title)
+                        .bold()
+                    
+                    HStack {
+                        Link(destination: URL(string: "https://rapidapi.com/natkapral/api/currency-converter5/")!) {
+                            Text("Currency Converter")
+                                .bold()
+                        }
+                        .offset(y: -10.5)
+                        Text("API provides all the data for chart generating.")
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                    .font(.callout)
+                    
+                    
+                    
+                    HStack {
+                        Link(destination: URL(string: "https://github.com/AppPear/ChartView")!) {
+                            Text("ChartView")
+                                .bold()
+                        }
+                        .offset(y: -10.5)
+                        Text("provides simple way to generate all the charts.")
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                    .font(.callout)
+                }
                 .padding(.bottom, 70)
                 
                 Text("Designed & Programmed by")
